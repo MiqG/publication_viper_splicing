refactored sf targets inference to fix small bugs and plot precision-recall

workflows/sf_targets_inference/scripts/evaluate_inference.py
@@ -117,7 +117,10 @@ def evaluate_prediction_kd(targets_true, targets_pred):
             
             # compute chi squared test
             tab = pd.crosstab(y_true, y_pred)
-            chisq_stat, chisq_pvalue, _, _ = stats.chi2_contingency(tab)
+            try:
+                chisq_stat, chisq_pvalue, _, _ = stats.chi2_contingency(tab)
+            except:
+                chisq_stat, chisq_pvalue = np.nan, np.nan
             
             # save
             eval_i = pd.Series(