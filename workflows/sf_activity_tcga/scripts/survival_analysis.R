c413c900dca644a09bb2814b11546e1dde6524b9
MiqG <miquelangladagirotto@gmail.com>
2023-07-14 19:46:14 +0200

    added gene expression analyses TCGA

--- workflows/sf_activity_tcga/scripts/survival_analysis.R
+++ workflows/sf_activity_tcga/scripts/survival_analysis.R
@@ -20,40 +20,70 @@ require(survminer)
 # PREP_DIR = file.path(ROOT,'data','prep')
 # SUPPORT_DIR = file.path(ROOT,"support")
 # RESULTS_DIR = file.path(ROOT,"results","sf_activity_tcga")
-# protein_activity_file = file.path(RESULTS_DIR,"files","protein_activity","LAML-PrimaryBloodDerivedCancerPeripheralBlood-EX.tsv.gz")
+# data_matrix_file = file.path(RESULTS_DIR,"files","protein_activity","LAML-PrimaryBloodDerivedCancerPeripheralBlood-EX.tsv.gz")
+# data_matrix_file = file.path(PREP_DIR,"genexpr_tpm","LAML-PrimaryBloodDerivedCancerPeripheralBlood.tsv.gz")
 # metadata_file = file.path(RAW_DIR,'UCSCXena','TCGA','phenotype','Survival_SupplementalTable_S1_20171025_xena_sp.gz')
 # surv_event_col = "OS"
 # surv_time_col = "OS.time"
 # sample_col = "sample"
-
+# features_oi_file = file.path(SUPPORT_DIR,"splicing_factors","splicing_factors-ensembl.txt")
 
 ##### FUNCTIONS #####
 run_cutpoint = function(df, surv_time_col, surv_event_col){
-    result_cut = surv_cutpoint(df, time=surv_time_col, event=surv_event_col, variables="activity")
-    result_cat = surv_categorize(result_cut)
-    fit_survdiff = survival::survdiff(
-        as.formula(sprintf("Surv(%s, %s) ~ activity", surv_time_col, surv_event_col)), 
-        data=result_cat
-    )
-    pvalue_survdiff = stats::pchisq(fit_survdiff[["chisq"]], length(fit_survdiff[["n"]]) - 1, lower.tail = FALSE)
-    fit_coxph = survival::coxph(
-        as.formula(sprintf("Surv(%s, %s) ~ activity", surv_time_col, surv_event_col)), 
-        data=df
-    )
-    result_surv = data.frame(
-        regulator = df[["regulator"]] %>% unique(),
-        activity_cutoff = result_cut[["activity"]][["estimate"]],
-        survdiff_pvalue = pvalue_survdiff,
-        survdiff_method = "Log-rank",
-        coxph_coef = fit_coxph[["coefficients"]],
-        coxph_pvalue = summary(fit_coxph)[["waldtest"]][["pvalue"]]
-    )
-    result_cat[["regulator"]] = df[["regulator"]] %>% unique()
-    result_cat[["sampleID"]] = df[["sampleID"]]
-    
-    result = list(
-        "surv" = result_surv,
-        "cat" = result_cat
+    result = tryCatch(
+        {
+            result_cut = surv_cutpoint(df, time=surv_time_col, event=surv_event_col, variables="feature_value")
+            result_cat = surv_categorize(result_cut)
+            fit_survdiff = survival::survdiff(
+                as.formula(sprintf("Surv(%s, %s) ~ feature_value", surv_time_col, surv_event_col)), 
+                data=result_cat
+            )
+            pvalue_survdiff = stats::pchisq(fit_survdiff[["chisq"]], length(fit_survdiff[["n"]]) - 1, lower.tail = FALSE)
+            fit_coxph = survival::coxph(
+                as.formula(sprintf("Surv(%s, %s) ~ feature_value", surv_time_col, surv_event_col)), 
+                data=df
+            )
+            result_surv = data.frame(
+                feature = df[["feature"]] %>% unique(),
+                feature_value_cutoff = result_cut[["feature_value"]][["estimate"]],
+                survdiff_pvalue = pvalue_survdiff,
+                survdiff_method = "Log-rank",
+                coxph_coef = fit_coxph[["coefficients"]],
+                coxph_pvalue = summary(fit_coxph)[["waldtest"]][["pvalue"]]
+            )
+            result_cat[["feature"]] = df[["feature"]] %>% unique()
+            result_cat[["sampleID"]] = df[["sampleID"]]
+
+            result = list(
+                "surv" = result_surv,
+                "cat" = result_cat
+            )
+            return(result)
+        }, error=function(cond){
+            result_surv = data.frame(
+                    feature = df[["feature"]] %>% unique(),
+                    feature_value_cutoff = NA,
+                    survdiff_pvalue = NA,
+                    survdiff_method = NA,
+                    coxph_coef = NA,
+                    coxph_pvalue = NA
+                )
+            
+            result_cat = data.frame(
+                    df[[surv_time_col]],
+                    df[[surv_event_col]],
+                    NA,
+                    df[["feature"]] %>% unique(),
+                    df[["sampleID"]]
+                )
+            colnames(result_cat) = c(surv_time_col, surv_event_col, "feature_value", "feature", "sampleID")
+            
+            
+            result = list(
+                "surv" = result_surv,
+                "cat" = result_cat
+            )
+        }
     )
     return(result)
 }
@@ -62,11 +92,12 @@ run_cutpoint = function(df, surv_time_col, surv_event_col){
 parseargs = function(){
     
     option_list = list( 
-        make_option("--protein_activity_file", type="character"),
+        make_option("--data_matrix_file", type="character"),
         make_option("--metadata_file", type="character"),
         make_option("--sample_col", type="character"),
         make_option("--surv_event_col", type="character"),
         make_option("--surv_time_col", type="character"),
+        make_option("--features_oi_file", type="character", default=NULL),
         make_option("--output_surv_file", type="character"),
         make_option("--output_cat_file", type="character")
     )
@@ -80,21 +111,36 @@ parseargs = function(){
 main = function(){
     args = parseargs()
     
-    protein_activity_file = args[["protein_activity_file"]]
+    data_matrix_file = args[["data_matrix_file"]]
     metadata_file = args[["metadata_file"]]
     sample_col = args[["sample_col"]]
     surv_event_col = args[["surv_event_col"]]
     surv_time_col = args[["surv_time_col"]]
+    features_oi_file = args[["features_oi_file"]]
     output_surv_file = args[["output_surv_file"]]
     output_cat_file = args[["output_cat_file"]]
     
     # load
-    protein_activity = read_tsv(protein_activity_file)
+    data_matrix = read_tsv(data_matrix_file)
     metadata = read_tsv(metadata_file)
     
+    # prep
+    ## rename first column
+    data_matrix = data_matrix %>%
+        dplyr::rename("feature"=colnames(data_matrix)[1])
+    
+    ## filter
+    if (!is.null(features_oi_file)){
+        print("Filtering features...")
+        features_oi = readLines(features_oi_file)
+        
+        data_matrix = data_matrix %>%
+            filter(feature %in% features_oi)
+    }
+    
     # compute KM into two groups
-    X = protein_activity %>%
-        pivot_longer(-regulator, names_to="sampleID", values_to="activity") %>%
+    X = data_matrix %>%
+        pivot_longer(-feature, names_to="sampleID", values_to="feature_value") %>%
         left_join(
             metadata[,c(sample_col, surv_event_col, surv_time_col)] %>% distinct(),
             by=c("sampleID"=sample_col)
@@ -102,8 +148,8 @@ main = function(){
         drop_na()
     
     result = lapply(
-            X %>% pull(regulator) %>% unique(), function(regulator_oi){
-            df = X %>% filter(regulator==regulator_oi)
+            X %>% pull(feature) %>% unique(), function(feature_oi){
+            df = X %>% filter(feature==feature_oi)
             res = run_cutpoint(df, surv_time_col, surv_event_col)
         })
     
