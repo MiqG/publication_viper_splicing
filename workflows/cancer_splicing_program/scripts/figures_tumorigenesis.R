9952ca12d2a68a11e4a17171830b847dc6f7479b
MiqG <miquelangladagirotto@gmail.com>
2023-09-26 18:03:05 +0200

    finished tumorigenesis figures

---
+++ workflows/cancer_splicing_program/scripts/figures_tumorigenesis.R
@@ -0,0 +1,210 @@
+#
+# Author: Miquel Anglada Girotto
+# Contact: miquel [dot] anglada [at] crg [dot] eu
+#
+
+require(optparse)
+require(tidyverse)
+require(ggpubr)
+require(cowplot)
+require(scattermore)
+require(extrafont)
+require(ggrepel)
+
+# variables
+THRESH_FDR = 0.05
+
+# formatting
+LINE_SIZE = 0.25
+
+FONT_SIZE = 2 # for additional labels
+FONT_FAMILY = "Arial"
+
+PAL_DRIVER_TYPE = c(
+    #"Non-driver"="lightgrey",
+    "Tumor suppressor"="#6C98B3",
+    "Oncogenic"="#F6AE2D"
+)
+
+# Development
+# -----------
+# ROOT = here::here()
+# RAW_DIR = file.path(ROOT,'data','raw')
+# PREP_DIR = file.path(ROOT,'data','prep')
+# SUPPORT_DIR = file.path(ROOT,"support")
+# RESULTS_DIR = file.path(ROOT,"results","cancer_splicing_program")
+# protein_activity_file = file.path(RESULTS_DIR,"files","protein_activity","tumorigenesis-EX.tsv.gz")
+# metadata_file = file.path(PREP_DIR,"metadata","tumorigenesis.tsv.gz")
+# driver_types_file = file.path(RESULTS_DIR,'files','PANCAN','cancer_program.tsv.gz')
+# figs_dir = file.path(RESULTS_DIR,"figures","tumorigenesis-EX")
+
+##### FUNCTIONS #####
+plot_tumorigenesis = function(protein_activity){
+    plts = list()
+    
+    X = protein_activity %>%
+        drop_na(driver_type) %>%
+        group_by(cell_line_name, driver_type, study_accession, GENE) %>%
+        summarize(activity = median(activity)) %>%
+        ungroup() %>%
+        filter(study_accession=="PRJNA193487") %>%
+        mutate(cell_line_name=factor(
+            cell_line_name, levels=c("BJ_PRIMARY","BJ_IMMORTALIZED",
+                                     "BJ_TRANSFORMED","BJ_METASTATIC")
+        ))
+    
+    plts[["tumorigenesis-cell_line_vs_activity-violin"]] = X %>%
+        filter(cell_line_name!="BJ_PRIMARY") %>%
+        ggplot(aes(x=cell_line_name, y=activity, group=interaction(cell_line_name,driver_type))) +
+        geom_violin(aes(fill=driver_type), color=NA, position=position_dodge(0.9)) +
+        geom_boxplot(width=0.1, outlier.size=0.1, fill=NA, color="black", position=position_dodge(0.9)) +
+        fill_palette(PAL_DRIVER_TYPE) +
+        stat_compare_means(method="wilcox.test", label="p.signif", size=FONT_SIZE, family=FONT_FAMILY) + 
+        theme_pubr() +
+        labs(x="Cell Line", y="Protein Activity", fill="Driver Type")
+    
+    return(plts)
+}
+
+
+make_plots = function(
+    protein_activiy
+){
+    plts = list(
+        plot_tumorigenesis(protein_activiy)
+    )
+    plts = do.call(c,plts)
+    return(plts)
+}
+
+
+make_figdata = function(
+    protein_activity
+){
+    figdata = list(
+        "tumorigenesis" = list(
+            "protein_activity" = protein_activity
+        )
+    )
+    return(figdata)
+}
+
+
+save_plt = function(plts, plt_name, extension='.pdf', 
+                    directory='', dpi=350, format=TRUE,
+                    width = par("din")[1], height = par("din")[2]){
+    print(plt_name)
+    plt = plts[[plt_name]]
+    if (format){
+        plt = ggpar(plt, font.title=8, font.subtitle=8, font.caption=8, 
+                    font.x=8, font.y=8, font.legend=6,
+                    font.tickslab=6, font.family=FONT_FAMILY, device=cairo_pdf)   
+    }
+    filename = file.path(directory,paste0(plt_name,extension))
+    save_plot(filename, plt, base_width=width, base_height=height, dpi=dpi, units='cm')
+}
+
+
+save_plots = function(plts, figs_dir){
+    save_plt(plts, "tumorigenesis-cell_line_vs_activity-violin", '.pdf', figs_dir, width=6, height=6)
+}
+
+
+save_figdata = function(figdata, dir){
+    lapply(names(figdata), function(x){
+        d = file.path(dir,'figdata',x)
+        dir.create(d, recursive=TRUE)
+        lapply(names(figdata[[x]]), function(nm){
+            df = figdata[[x]][[nm]]
+            filename = file.path(d, paste0(nm,'.tsv.gz'))
+            write_tsv(df, filename)
+            
+            print(filename)
+        })
+    })
+}
+
+
+parseargs = function(){
+    
+    option_list = list( 
+        make_option("--protein_activity_file", type="character"),
+        make_option("--metadata_file", type="character"),
+        make_option("--driver_types_file", type="character"),
+        make_option("--figs_dir", type="character")
+    )
+
+    args = parse_args(OptionParser(option_list=option_list))
+    
+    return(args)
+}
+
+
+main = function(){
+    args = parseargs()
+    
+    protein_activity_file = args[["protein_activity_file"]]
+    metadata_file = args[["metadata_file"]]
+    driver_types_file = args[["driver_types_file"]]
+    figs_dir = args[["figs_dir"]]
+    
+    dir.create(figs_dir, recursive = TRUE)
+    
+    # load
+    protein_activity = read_tsv(protein_activity_file)
+    metadata = read_tsv(metadata_file)
+    driver_types = read_tsv(driver_types_file)
+    
+    # prep
+    protein_activity = protein_activity %>%
+        pivot_longer(-regulator, names_to="sampleID", values_to="activity") %>%
+        left_join(metadata, by="sampleID") %>%
+        drop_na(condition, activity) %>%
+        mutate(
+            condition_lab = sprintf(
+                "%s (%s%s) (%s%s) | %s | %s", condition, pert_time, pert_time_units, 
+                pert_concentration, pert_concentration_units, cell_line_name, study_accession
+            )
+        ) %>%
+        
+        # summarize replicates
+        group_by(condition_lab, condition, pert_time, pert_time_units, 
+                 pert_concentration, pert_concentration_units, cell_line_name, study_accession,
+                 PERT_ENSEMBL, PERT_GENE, regulator) %>%
+        summarize(
+            activity = median(activity, na.rm=TRUE),
+            abs_activity = abs(activity),
+        ) %>%
+        ungroup() %>%
+        
+        # add activity
+        group_by(condition_lab) %>%
+        arrange(activity) %>%
+        mutate(
+            activity_ranking = row_number(),
+            total_avail_sfs = sum(regulator %in% unlist(strsplit(PERT_ENSEMBL, ",")))
+        ) %>%
+        arrange(abs_activity) %>%
+        mutate(
+            abs_activity_ranking = row_number(),
+        ) %>%
+        ungroup() %>%
+        left_join(driver_types, by=c("regulator"="ENSEMBL"))
+    
+    # plot
+    plts = make_plots(protein_activity)
+    
+    # make figdata
+    figdata = make_figdata(protein_activity)
+
+    # save
+    save_plots(plts, figs_dir)
+    save_figdata(figdata, figs_dir)
+}
+
+
+##### SCRIPT #####
+if (sys.nframe() == 0L) {
+    main()
+    print("Done!")
+}
---
+++ workflows/cancer_splicing_program/tumorigenesis.smk
@@ -0,0 +1,126 @@
+"""
+Author: Miquel Anglada Girotto
+Contact: miquelangladagirotto [at] gmail [dot] com
+
+Outline
+-------
+- study activities of cancer-driver splicing program 
+    - during tumorigenesis in brain organoids
+    - upon cancer-driver mutations
+"""
+
+import os
+
+# variables
+ROOT = os.path.dirname(os.path.dirname(os.getcwd()))
+RAW_DIR = os.path.join(ROOT,"data","raw")
+PREP_DIR = os.path.join(ROOT,"data","prep")
+SUPPORT_DIR = os.path.join(ROOT,"support")
+BIN_DIR = os.path.join(ROOT,"bin")
+RESULTS_DIR = os.path.join(ROOT,"results","cancer_splicing_program")
+REGULONS_DIR = os.path.join(ROOT,"results","regulon_inference")
+SAVE_PARAMS = {"sep":"\t", "index":False, "compression":"gzip"}
+
+EVENT_TYPES = ["EX"]
+OMIC_TYPES = EVENT_TYPES
+
+DATASETS = ["tumorigenesis"]
+
+###### RULES ######
+rule all:
+    input:
+        # compute signatures
+        expand(os.path.join(RESULTS_DIR,"files","signatures","{dataset}-{omic_type}.tsv.gz"), dataset=DATASETS, omic_type=OMIC_TYPES),
+        # compute protein activities
+        expand(os.path.join(RESULTS_DIR,"files","protein_activity","{dataset}-{omic_type}.tsv.gz"), dataset=DATASETS, omic_type=OMIC_TYPES),
+        
+        # figures
+        #expand(os.path.join(RESULTS_DIR,"figures","tumorigenesis-{omic_type}"), omic_type=OMIC_TYPES)
+        
+
+rule compute_signatures:
+    input:
+        metadata = os.path.join(PREP_DIR,'metadata','tumorigenesis.tsv.gz'),
+        splicing = os.path.join(PREP_DIR,'event_psi','tumorigenesis-{omic_type}.tsv.gz'),
+    output:
+        signatures = os.path.join(RESULTS_DIR,"files","signatures","tumorigenesis-{omic_type}.tsv.gz")
+    run:
+        import pandas as pd
+        
+        metadatas = []
+        signatures = []
+        
+        # load
+        metadata = pd.read_table(input.metadata)
+        splicing = pd.read_table(input.splicing, index_col=0)
+        
+        # subset
+        common_samples = set(metadata["sampleID"]).intersection(splicing.columns)
+        metadata = metadata.loc[metadata["sampleID"].isin(common_samples)].copy()
+        splicing = splicing[common_samples].copy()
+        
+        # delta PSI as the difference between conditions and the mean of the conditions
+        signatures = {}
+        for sample_oi in metadata["sampleID"]:
+            # get the controls of the sample
+            ctls = metadata.loc[metadata["sampleID"]==sample_oi, "control_samples"].values[0]
+            
+            # controls will be np.nan
+            if isinstance(ctls,str):
+                ctls = ctls.split("||")
+                
+                # there may be empty controls
+                if any(splicing.columns.isin(ctls)):
+                    psi_ctls = splicing.loc[:,splicing.columns.isin(ctls)].mean(axis=1)
+
+                    # compute delta PSI
+                    dpsi = splicing[sample_oi] - psi_ctls
+
+                    signatures[sample_oi] = dpsi
+
+                    del dpsi, psi_ctls, ctls
+                else:
+                    print(splicing.columns, ctls)
+                    print(splicing.columns.isin(ctls))
+                    continue
+
+        signatures = pd.DataFrame(signatures)
+        
+        # save
+        signatures.reset_index().to_csv(output.signatures, **SAVE_PARAMS)
+        
+        print("Done!")
+        
+        
+rule compute_protein_activity:
+    input:
+        signature = os.path.join(RESULTS_DIR,"files","signatures","{dataset}-{omic_type}.tsv.gz"),
+        regulons_path = os.path.join(REGULONS_DIR,"files","experimentally_derived_regulons_pruned-{omic_type}")
+    output:
+        os.path.join(RESULTS_DIR,"files","protein_activity","{dataset}-{omic_type}.tsv.gz")
+    params:
+        script_dir = BIN_DIR
+    shell:
+        """
+        Rscript {params.script_dir}/compute_protein_activity.R \
+                    --signature_file={input.signature} \
+                    --regulons_path={input.regulons_path} \
+                    --output_file={output}
+       """
+
+        
+rule make_figures:
+    input:
+        protein_activity = os.path.join(RESULTS_DIR,"files","protein_activity","tumorigenesis-{omic_type}.tsv.gz"),
+        metadata = os.path.join(RESULTS_DIR,"files","protein_activity","tumorigenesis-{omic_type}.tsv.gz"),
+        driver_types = os.path.join(RESULTS_DIR,'files','PANCAN','cancer_program.tsv.gz')
+    output:
+        directory(os.path.join(RESULTS_DIR,"figures","tumorigenesis-{omic_type}"))
+    shell:
+        """
+        Rscript scripts/figures_tumorigenesis.R \
+                    --protein_activity_file={input.protein_activity} \
+                    --metadata_file={input.metadata} \
+                    --driver_types={input.driver_types} \
+                    --figs_dir={output}
+        """
